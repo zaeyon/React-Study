02a8a346a64ecca694b17f3b9cd97b0d9a8823e9
이재연 <zaeyon@ijaeyeon-ui-MacBookAir.local>
2023-01-24 16:03:29 +0900

    props & state, eventhandling

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,17 +1,8 @@
 import React from 'react';
-import './App.css';
+import EventPractice from './EventPractice';
 
-function App() {
-  const name = '리액트';
-
-  return (
-    <div>
-      <div className="react">{name}</div>
-      <h1>들여쓰기가 이상한</h1>
-      <h2>코드</h2>
-      <p>입니다.</p>
-    </div>
-  );
-}
+const App = () => {
+  return <EventPractice />;
+};
 
 export default App;
