dca3091e914b1e5a0f9f32ddb4ddb7aa73daf65d
이재연 <wodus5677@gmail.com>
2023-02-01 18:04:22 +0900

    Commit before yarn eject

---
+++ styling-react2/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import SassComponent from "./SassComponent.js";
+
+function App() {
+  return (
+    <div>
+      <SassComponent />
+    </div>
+  );
+}
+
+export default App;
